abfae1c9dd11a19d816a7fbf18b1896c8fa85e45
serkanXman <msozkilic@gmail.com>
2022-11-27 19:32:52 +0100

    saucedemo sitesi test edildi

--- src/CalısmaAlanı/Deneme.java
+++ src/CalısmaAlanı/Deneme.java
@@ -1,36 +1,51 @@
 package CalısmaAlanı;
-
-
-
+import Utils.BaseStaticDriver;
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.time.Duration;
 import java.util.List;
 
-public class Deneme {
-    public static void main(String[] args)  {
+public class Deneme extends BaseStaticDriver {
+    public static void main(String[] args) {
+
+     /*   Senaryo
+        1-https://www.saucedemo.com/ sitesini aciniz
+        2-Login islemi yapiniz
+        3-Buradaki urunlerin isimlerini ekrana yazdirinuz*/
+        driver.get("https://www.saucedemo.com/");
+
+        WebElement username=driver.findElement(By.id("user-name"));
+        username.sendKeys("problem_user");
+
+        WebElement password=driver.findElement(By.id("password"));
+        password.sendKeys("secret_sauce");
+
+        WebElement loginButton=driver.findElement(By.id("login-button"));
+        loginButton.click();
+
+        List<WebElement>product=driver.findElements(By.className("inventory_container"));
+        for (WebElement e:product
+        ) {
+            System.out.println(e.getText());
+
+        }
+
+        /*List<WebElement>product=driver.findElements(By.id("inventory_list"));
+        for (WebElement e:product
+             ) {
+            System.out.println(e.getText());
+
+        }*/
+
+        BekleKapat();
 
 
     }
-    public static WebDriver driver;
-    public static void driveriAc(){
-        System.setProperty("webdriver.chrome.driver","driver/chromedriver.exe");
-         driver=new ChromeDriver();
-        driver.manage().deleteAllCookies();
-        driver.manage().window().maximize();
-
-        Duration duration=Duration.ofSeconds(30);
-        driver.manage().timeouts().pageLoadTimeout(duration);
-        driver.manage().timeouts().implicitlyWait(duration);
-
-    }public static void bekleKapat()  {
-        try {
-            Thread.sleep(3000);
-        }catch (InterruptedException e){
-            throw new RuntimeException(e);
-
-        }driver.quit();
 
     }
-}
+

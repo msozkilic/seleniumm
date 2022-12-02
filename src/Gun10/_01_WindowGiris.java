71c9e347c3d6e2c9c59d7a32e9a545d4425a4b79
serkanXman <msozkilic@gmail.com>
2022-12-02 16:36:59 +0100

    window

---
+++ src/Gun10/_01_WindowGiris.java
@@ -0,0 +1,27 @@
+package Gun10;
+
+import Utils.BaseStaticDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+import java.util.Set;
+
+import static io.netty.util.internal.SystemPropertyUtil.contains;
+
+public class _01_WindowGiris extends BaseStaticDriver {
+    public static void main(String[] args) {
+        driver.get("https://www.selenium.dev/documentation/");
+
+        List<WebElement> linlkler = driver.findElements(By.cssSelector("a[target='_blank'"));
+        for (WebElement link : linlkler) {
+            if(link.getAttribute("href").contains("mailto"))
+                link.click();
+                BekleKapat();
+        }
+       Set<String> windosIdler= driver.getWindowHandles();
+        for (String id:windosIdler) {
+            System.out.println("id "+ id);
+        }
+    }
+}

04e0e232de05e84989bbded318c7de1466b01319
serkanXman <msozkilic@gmail.com>
2022-12-04 18:38:14 +0100

    robot Class devam

--- src/Gun11/_01_RobotGiris.java
+++ src/Gun11/_01_RobotGiris.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.awt.*;
+import java.awt.datatransfer.StringSelection;
 import java.awt.event.KeyEvent;
 import java.time.Duration;
 
@@ -15,6 +16,7 @@ public class _01_RobotGiris extends BaseStaticDriver {
     public static void main(String[] args) throws AWTException {
         driver.get("https://demo.guru99.com/test/upload/");
 
+        //todo frame in varligi kontrol edilip var ise veya yok ise her iki duruma göre cözüm yapilacak
         driver.switchTo().frame(8);
         WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement acceptAll=wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()=''")));
@@ -29,6 +31,15 @@ public class _01_RobotGiris extends BaseStaticDriver {
         }
         rbt.keyPress(KeyEvent.VK_ENTER); //todo tusa basildi
         rbt.keyRelease(KeyEvent.VK_ENTER);//todo tus birakildi.
+
+        StringSelection stringSelection=new StringSelection("C:\\Users\\TechnoStudy\\Desktop\\ornek.txt");
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringSelection,null);
+
+        rbt.keyPress(KeyEvent.VK_CONTROL);
+        rbt.keyPress(KeyEvent.VK_V);
+
+        rbt.keyRelease(KeyEvent.VK_CONTROL);
+        rbt.keyRelease(KeyEvent.VK_V);
 
         BekleKapat();
     }

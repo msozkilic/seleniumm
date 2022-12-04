9c753d0ff0b6ca05d6e21fd28f0b849d43fd562c
serkanXman <msozkilic@gmail.com>
2022-12-04 10:41:12 +0100

    window scrolldown

---
+++ src/Gun10/_05_ScrollToBottomOfThePage.java
@@ -0,0 +1,15 @@
+package Gun10;
+
+import Utils.BaseStaticDriver;
+import org.openqa.selenium.JavascriptExecutor;
+
+public class _05_ScrollToBottomOfThePage extends BaseStaticDriver {
+    public static void main(String[] args) {
+        driver.get("https://triplebyte.com");
+
+        JavascriptExecutor js=(JavascriptExecutor) driver;
+        js.executeScript("window.scrollTo(0,document.body.scrollHeight)");
+
+        BekleKapat();
+    }
+}
